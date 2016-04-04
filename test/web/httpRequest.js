f412c7786c2c821313020a8337903db076b48f19
Balard Cedric <cedric.balard@yocto.re>
2016-04-04 11:16:41 +0400

    Set all unit-test into async process

--- test/web/httpRequest.js
+++ test/web/httpRequest.js
@@ -18,75 +18,8 @@ describe('httpRequests', function(doneDescribe) {
 
   app = server.createServer(port);
 
+  // check if app is started
   if (app) {
-    // should retrieve user
-    it('GET : returns all user - should return 200 and json object', function(done) {
-      superagent
-      .get(url + '/account').end(function(err, res) {
-        assert.ifError(err);
-        assert.equal(res.body.code, 200000);
-        done();
-      });
-    });
-
-    // should retrieve user
-    it('HEAD : returns heand of all user - should return 200 and json object', function(done) {
-      superagent
-      .get(url + '/account').end(function(err, res) {
-        assert.ifError(err);
-        assert.equal(res.body.code, 200000);
-        done();
-      });
-    });
-
-    // should not post because body is xrong
-    it('POST : should not create user because body is empty', function(done) {
-      superagent
-      .post(url + '/account')
-      .end(function(err, res) {
-        assert.ifError(err);
-        assert.equal(res.body.code, 400000);
-        done();
-      });
-    });
-
-    it('PATCH : should not patch user because id was not specified', function(done) {
-      superagent
-      .patch(url + '/account')
-      .send({
-        name      : 'aaaa',
-        test      : 'bbbb'
-      })
-      .end(function(err, res) {
-        assert.ifError(err);
-        assert.equal(res.body.code, 400000);
-        done();
-      });
-    });
-
-    it('PUT : should not put user because id was not specified', function(done) {
-      superagent
-      .put(url + '/account')
-      .send({
-        name      : 'aaaa',
-        test      : 'bbbb'
-      })
-      .end(function(err, res) {
-        assert.ifError(err);
-        assert.equal(res.body.code, 400000);
-        done();
-      });
-    });
-
-    it('DELETE : should not delete user because id was not specified', function(done) {
-      superagent
-      .delete(url + '/account')
-      .end(function(err, res) {
-        assert.ifError(err);
-        assert.equal(res.body.code, 400000);
-        done();
-      });
-    });
 
     var accountId = '';
     /*******************************************************/
@@ -97,9 +30,107 @@ describe('httpRequests', function(doneDescribe) {
 
       function (callback) {
 
-        describe('POST account : ', function () {
+        describe('GET method : ', function () {
 
-          it('POST : should create user because body is wrong', function(done) {
+          // should retrieve user
+          it('returns all user - should return 200 and json object', function(done) {
+            superagent
+            .get(url + '/account').end(function(err, res) {
+              assert.ifError(err);
+              assert.equal(res.body.code, 200000);
+              done();
+              callback(null);
+            });
+          });
+        });
+      },function (callback) {
+        describe('HEAD method : ', function () {
+
+          // should retrieve user
+          it('returns heand of all user - should return 200 and json object', function(done) {
+            superagent
+            .get(url + '/account').end(function(err, res) {
+              assert.ifError(err);
+              assert.equal(res.body.code, 200000);
+              done();
+              callback(null);
+
+            });
+          });
+        });
+      },function (callback) {
+        describe('POST method : ', function () {
+
+          // should not post because body is xrong
+          it('should not create user because body is empty', function(done) {
+            superagent
+            .post(url + '/account')
+            .end(function(err, res) {
+              assert.ifError(err);
+              assert.equal(res.body.code, 400000);
+              done();
+              callback(null);
+
+            });
+          });
+        });
+      },function (callback) {
+        describe('PATCH method : ', function () {
+
+          it('should not patch user because id was not specified', function(done) {
+            superagent
+            .patch(url + '/account')
+            .send({
+              name      : 'aaaa',
+              test      : 'bbbb'
+            })
+            .end(function(err, res) {
+              assert.ifError(err);
+              assert.equal(res.body.code, 400000);
+              done();
+              callback(null);
+
+            });
+          });
+        });
+      },function (callback) {
+        describe('PUT method :', function () {
+
+          it('should not put user because id was not specified', function(done) {
+            superagent
+            .put(url + '/account')
+            .send({
+              name      : 'aaaa',
+              test      : 'bbbb'
+            })
+            .end(function(err, res) {
+              assert.ifError(err);
+              assert.equal(res.body.code, 400000);
+              done();
+              callback(null);
+
+            });
+          });
+        });
+      },function (callback) {
+        describe('DELETE method : ', function () {
+
+          it('should not delete user because id was not specified', function(done) {
+            superagent
+            .delete(url + '/account')
+            .end(function(err, res) {
+              assert.ifError(err);
+              assert.equal(res.body.code, 400000);
+              done();
+              callback(null);
+            });
+          });
+        });
+      },function (callback) {
+
+        describe('POST method : ', function () {
+
+          it('should create user because body is wrong', function(done) {
             superagent
             .post(url + '/account')
             .send({
@@ -120,7 +151,7 @@ describe('httpRequests', function(doneDescribe) {
       },
       function (callback) {
 
-        describe('Get account : ', function () {
+        describe('GET method : ', function () {
 
           it('sould get account created with id : ' + accountId, function (done) {
             superagent
@@ -135,7 +166,7 @@ describe('httpRequests', function(doneDescribe) {
         });
       },function (callback) {
 
-        describe('PATCH account : ', function () {
+        describe('PATCH method : ', function () {
 
           it('sould patch account created with id : ' + accountId, function (done) {
             superagent
@@ -154,7 +185,7 @@ describe('httpRequests', function(doneDescribe) {
         });
       },function (callback) {
 
-        describe('PUT account : ', function () {
+        describe('PUT method : ', function () {
 
           it('sould put account created with id : ' + accountId, function (done) {
             superagent
@@ -173,7 +204,7 @@ describe('httpRequests', function(doneDescribe) {
         });
       },function (callback) {
 
-        describe('DELETE account : ', function () {
+        describe('DELETE method : ', function () {
 
           it('sould not delete account because deleted_date is not specified : ', function (done) {
             superagent
@@ -190,10 +221,8 @@ describe('httpRequests', function(doneDescribe) {
       }
     ], function (err, results) {
       // close server
-      after(function() {
-        app.close();
-        server.closeServer()
-      });
+      app.close();
+      server.closeServer();
     });
   }
 });
